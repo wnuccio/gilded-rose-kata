9e5641d27356428e3c7ceb6b8679fb101fb1c10d
wnuccio <walter.nuccio@it.clara.net>
2020-10-05 15:39:03 +0200

    refactor updateQuality by separating logic for each item type

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,62 +1,77 @@
 package com.gildedrose;
 
+import java.util.stream.Stream;
+
 class GildedRose {
     Item[] items;
+    private String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+    private String AGED_BRIE = "Aged Brie";
+    private Item item;
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        for (Item value : items) {
+            this.item = value;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+            if (is(SULFURAS)) {
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+            } else if (is(AGED_BRIE)) {
+                increaseQualityNotOver50(sellDatePassed() ? 2 : 1);
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+            } else if (is(BACKSTAGE_PASS)) {
+                if (thereAreDays(11, Integer.MAX_VALUE)) {
+                    increaseQualityNotOver50(1);
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
+                } else if (thereAreDays(6, 10)) {
+                    increaseQualityNotOver50(2);
+
+                } else if (thereAreDays(1, 5)) {
+                    increaseQualityNotOver50(3);
+
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                    item.quality = 0;
                 }
+
+            } else {
+                reduceQualityNotUnderZero(sellDatePassed() ? 2 : 1);
             }
+
+            reduceSellInByOne();
         }
+    }
+
+    private boolean sellDatePassed() {
+        return item.sellIn <= 0;
+    }
+
+    private boolean thereAreDays(int min, int max) {
+        return item.sellIn >= min && item.sellIn <= max;
+    }
+
+    private void reduceSellInByOne() {
+        item.sellIn = item.sellIn - 1;
+    }
+
+    private void increaseQualityNotOver50(int increment) {
+        if (item.quality < 50)
+            item.quality = item.quality + increment;
+    }
+
+    private boolean is(String... names) {
+        return Stream.of(names)
+                .anyMatch(name -> this.item.name.equals(name));
+    }
+
+    private void reduceQualityNotUnderZero(int decrement) {
+        if (item.quality > 0)
+            item.quality = item.quality - decrement;
+    }
+
+    private boolean thereAreXDaysOrLess(int maxSellIn) {
+        return item.sellIn <= maxSellIn;
     }
 }
